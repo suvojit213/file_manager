ad22e5a91574894915b882bd44d16a3a170ce5e9
suvojit213 <suvojitsengupta21@gmail.com>
2025-07-22 18:49:19 +0530

    feat: enhanced text editor with view/edit modes and search

--- lib/screens/text_editor_screen.dart
+++ lib/screens/text_editor_screen.dart
@@ -22,6 +22,10 @@ class _TextEditorScreenState extends State<TextEditorScreen> {
   String _fileContent = '';
   String _initialContent = '';
   bool _hasChanges = false;
+  bool _isEditMode = false;
+  final TextEditingController _searchController = TextEditingController();
+  List<int> _searchResults = [];
+  int _currentSearchIndex = -1;
 
   @override
   void initState() {
@@ -33,6 +37,7 @@ class _TextEditorScreenState extends State<TextEditorScreen> {
   @override
   void dispose() {
     _controller.dispose();
+    _searchController.dispose();
     super.dispose();
   }
 
@@ -83,6 +88,58 @@ class _TextEditorScreenState extends State<TextEditorScreen> {
     }
   }
 
+  void _toggleEditMode() {
+    setState(() {
+      _isEditMode = !_isEditMode;
+      if (!_isEditMode && _hasChanges) {
+        _saveFileContent();
+      }
+    });
+  }
+
+  void _search() {
+    final query = _searchController.text;
+    if (query.isEmpty) {
+      setState(() {
+        _searchResults = [];
+        _currentSearchIndex = -1;
+      });
+      return;
+    }
+    final content = _fileContent.toLowerCase();
+    final results = <int>[];
+    int startIndex = 0;
+    while (startIndex < content.length) {
+      final index = content.indexOf(query.toLowerCase(), startIndex);
+      if (index == -1) {
+        break;
+      }
+      results.add(index);
+      startIndex = index + query.length;
+    }
+    setState(() {
+      _searchResults = results;
+      _currentSearchIndex = results.isNotEmpty ? 0 : -1;
+    });
+  }
+
+  void _goToNextSearchResult() {
+    if (_searchResults.isNotEmpty) {
+      setState(() {
+        _currentSearchIndex = (_currentSearchIndex + 1) % _searchResults.length;
+      });
+    }
+  }
+
+  void _goToPreviousSearchResult() {
+    if (_searchResults.isNotEmpty) {
+      setState(() {
+        _currentSearchIndex =
+            (_currentSearchIndex - 1 + _searchResults.length) % _searchResults.length;
+      });
+    }
+  }
+
   String _getLanguage() {
     final extension = p.extension(widget.file.path).toLowerCase();
     switch (extension) {
@@ -118,41 +175,155 @@ class _TextEditorScreenState extends State<TextEditorScreen> {
       appBar: AppBar(
         title: Text(widget.file.name),
         actions: [
-          if (_hasChanges)
-            IconButton(
-              icon: const Icon(Icons.save),
-              onPressed: _saveFileContent,
-            ),
+          IconButton(
+            icon: Icon(_isEditMode ? Icons.save : Icons.edit),
+            onPressed: _toggleEditMode,
+          ),
+          PopupMenuButton<String>(
+            onSelected: (value) {
+              if (value == 'search') {
+                _showSearchDialog();
+              }
+            },
+            itemBuilder: (BuildContext context) {
+              return [
+                const PopupMenuItem<String>(
+                  value: 'search',
+                  child: Text('Search'),
+                ),
+              ];
+            },
+          ),
         ],
       ),
       body: _isLoading
           ? const Center(child: CircularProgressIndicator())
-          : Stack(
+          : Column(
               children: [
-                SingleChildScrollView(
-                  child: HighlightView(
-                    _fileContent,
-                    language: _getLanguage(),
-                    theme: githubTheme,
-                    padding: const EdgeInsets.all(12),
-                    textStyle: textStyle,
-                  ),
+                Expanded(
+                  child: _isEditMode
+                      ? TextField(
+                          controller: _controller,
+                          expands: true,
+                          maxLines: null,
+                          keyboardType: TextInputType.multiline,
+                          style: textStyle,
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.all(12),
+                          ),
+                        )
+                      : _buildHighlightView(textStyle),
                 ),
-                SingleChildScrollView(
-                  child: TextField(
-                    controller: _controller,
-                    expands: true,
-                    maxLines: null,
-                    keyboardType: TextInputType.multiline,
-                    style: textStyle.copyWith(color: Colors.transparent),
-                    decoration: const InputDecoration(
-                      border: InputBorder.none,
-                      contentPadding: EdgeInsets.all(12),
-                    ),
-                  ),
-                ),
+                if (_searchResults.isNotEmpty) _buildSearchControls(),
               ],
             ),
+    );
+  }
+
+  Widget _buildHighlightView(TextStyle textStyle) {
+    if (_searchResults.isEmpty) {
+      return SingleChildScrollView(
+        child: HighlightView(
+          _fileContent,
+          language: _getLanguage(),
+          theme: githubTheme,
+          padding: const EdgeInsets.all(12),
+          textStyle: textStyle,
+        ),
+      );
+    }
+    final query = _searchController.text;
+    final spans = <TextSpan>[];
+    int lastMatchEnd = 0;
+    for (int i = 0; i < _searchResults.length; i++) {
+      final matchIndex = _searchResults[i];
+      if (matchIndex > lastMatchEnd) {
+        spans.add(TextSpan(text: _fileContent.substring(lastMatchEnd, matchIndex)));
+      }
+      spans.add(
+        TextSpan(
+          text: _fileContent.substring(matchIndex, matchIndex + query.length),
+          style: TextStyle(
+            backgroundColor: i == _currentSearchIndex ? Colors.yellow : Colors.orange,
+          ),
+        ),
+      );
+      lastMatchEnd = matchIndex + query.length;
+    }
+    if (lastMatchEnd < _fileContent.length) {
+      spans.add(TextSpan(text: _fileContent.substring(lastMatchEnd)));
+    }
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: RichText(
+          text: TextSpan(
+            style: textStyle.copyWith(color: Colors.black),
+            children: spans,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _showSearchDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Search'),
+          content: TextField(
+            controller: _searchController,
+            autofocus: true,
+            decoration: const InputDecoration(hintText: 'Enter search term...'),
+            onChanged: (value) => _search(),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Done'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildSearchControls() {
+    return Container(
+      color: Colors.grey[200],
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text('${_currentSearchIndex + 1} of ${_searchResults.length}'),
+          Row(
+            children: [
+              IconButton(
+                icon: const Icon(Icons.arrow_upward),
+                onPressed: _goToPreviousSearchResult,
+              ),
+              IconButton(
+                icon: const Icon(Icons.arrow_downward),
+                onPressed: _goToNextSearchResult,
+              ),
+              IconButton(
+                icon: const Icon(Icons.close),
+                onPressed: () {
+                  setState(() {
+                    _searchController.clear();
+                    _searchResults = [];
+                    _currentSearchIndex = -1;
+                  });
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
